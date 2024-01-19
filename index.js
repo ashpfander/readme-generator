b717e945364f2518233a18a9ba828ef5eb3311b0
Ashleigh Pfander <ashleighharvey@Ashleighs-MBP.attlocal.net>
2024-01-19 15:00:22 -0500

    added in questions

--- index.js
+++ index.js
@@ -6,7 +6,64 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    inquirer
+    .prompt([
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Please write a description about this project.',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What are the steps to installing this project?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'How is this project intended to be used?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'How may others contribute to this project, if at all?',
+        name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: 'How might others test this application?',
+        name: 'test',
+    },
+    {
+        type: 'list',
+        message: 'Select a license for this project.',
+        name: 'license',
+        choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense'],
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'email',
+    },
+  ])
+//   // Uses fs module to create the README.md file
+//   .then((answers) => {
+//     const createREADME = generateMarkdown(answers);
+
+//     fs.writeFile('README.md', createREADME, (err) =>
+//     err ? console.error(err) : console.log('README.md succesfully created!'));
+//   })
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
